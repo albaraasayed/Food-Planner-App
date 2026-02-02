fb3ac12c16a4a591301cbe47c5ea236773a5503c
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-02-02 14:03:12 +0200

    Add Splash Screen

---
+++ app/src/main/java/com/example/foodplannerapp/ui/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.foodplannerapp.ui;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.foodplannerapp.R;
+
+public class SplashScreen extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_splash_screen, container, false);
+        LottieAnimationView lottie = view.findViewById(R.id.lottieSplash);
+
+        lottie.playAnimation();
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            if (isAdded()) {
+                NavHostFragment.findNavController(this)
+                        .navigate(R.id.action_splash_to_home);
+            }
+        }, 3000);
+
+
+        return view;
+    }
+}
